6eb6b835db8f856e1dfb9ca0ba31fc5404a1e1d5
ZumDeWald <zumdewald@gmail.com>
2018-07-31 22:49:29 -0500

    Cat Clicker Premium initial commit
    5 cats created with OOP
    Cats pushed to array and added to list

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,8 @@
 //CatClicker JS
 $(document).ready(() => {
 
-  // //Submit Clicker
-  // const clicks = $(".clicks");
-  //
-  // let clickCount = 0;
-  //
-  // $(".cat-button-article").on("click", ".cat-image", () => {
-  //   clickCount++;
-  //   clicks.text(clickCount);
-  // });
-  //
+ //Cat Array
+ const cats = [];
 
  //Cat Class
  class Cat {
@@ -27,7 +19,16 @@ $(document).ready(() => {
      };
 
 
-   buildDOMCat() {
+   addToCatList() {
+     $(".cat-list").append(
+       `<li class="cat-name">${this.name}</li>`
+     );
+   }
+
+   buildCatDOM() {
+     //Clear Cat Area
+     $(".section-container").html("");
+
      $(".section-container").append(
        `<article class="cat-button-article">
 
@@ -45,12 +46,35 @@ $(document).ready(() => {
  //Cats
  const sylvester = new Cat("sylvester", "Sylvester", "https://cdn.shopify.com/s/files/1/0673/5325/files/LadyDinahs_Cat_Cafe_Alice-Block_2048x.jpg?v=1517059999");
 
- const grumpy = new Cat("grumpy", "Grumpy", "https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg");
+ const grumpy = new Cat("grumpy", "Grumpy Cat", "https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg");
+
+ const avenger = new Cat("avenger", "Avenger", "https://catzone-tcwebsites.netdna-ssl.com/wp-content/uploads/2015/06/grey-cat-names.jpg");
+
+ const sweetPea = new Cat("sweetPea", "Sweet Pea", "https://media.mnn.com/assets/images/2018/02/AdorableBlackCatLookingAtCameraFromSofa.jpg.653x0_q80_crop-smart.jpg");
+
+ const orb = new Cat("orb", "Orb Kitty", "https://i.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg");
 
 
- //Elements for each Cat
- sylvester.buildDOMCat();
- grumpy.buildDOMCat();
+
+ //Push Cats into cats Array
+ cats.push(sylvester, grumpy, avenger, sweetPea, orb);
+ console.log(cats);
+
+
+
+ //Add cats to Cat List
+ for (cat of cats) {
+   let catListName = $(".cat-list").append(
+     `<li class="cat-name">${cat.name}</li>`
+   );
+
+   catListName.on("click", (function(catCopy) {
+     return function() {
+       catCopy.buildCatDOM();
+     };
+   }) (cat) );
+ }
+
 
 
  //Click Functions
@@ -59,6 +83,7 @@ $(document).ready(() => {
    sylvester.increaseClick();
  });
 
+ //Grumpy
  $(".cat-button-article").on("click", "#grumpy", (e) => {
    grumpy.increaseClick();
  });
