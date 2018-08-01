4450e3e3c6aefa6740c1971d127bff42aa8f2257
ZumDeWald <zumdewald@gmail.com>
2018-07-31 21:29:38 -0500

    Cat Clicker phase 2 complete
    2 cats
    Clicks counted for each and update

--- scripts/app.js
+++ scripts/app.js
@@ -14,27 +14,27 @@ $(document).ready(() => {
 
  //Cat Class
  class Cat {
-   constructor(name, pic) {
+   constructor(id, name, pic) {
+     this.id = id;
      this.name = name;
      this.pic = pic;
      this.clicks = 0;
    };
 
    increaseClick() {
-     $(this.pic).click(() => {
        this.clicks ++;
-       clicks.text(clickCount);
-     });
-   };
+       $(`.${this.id}`).text(this.clicks);
+     };
+
 
    buildDOMCat() {
      $(".section-container").append(
        `<article class="cat-button-article">
 
-       <img class="cat-image" src="${this.pic}"
-       alt="${this.name}">
+         <img id="${this.id}" class="cat-image" src="${this.pic}"
+         alt="${this.name}">
 
-         <h4 class="name">${this.name}<span class="clicks">${this.clicks}</span></h4>
+         <h4 class="name">${this.name}<span class="clicks ${this.id}">${this.clicks}</span></h4>
 
      </article>`
    )};
@@ -43,21 +43,26 @@ $(document).ready(() => {
 
 
  //Cats
- const sylvester = new Cat("Sylvester", "https://cdn.shopify.com/s/files/1/0673/5325/files/LadyDinahs_Cat_Cafe_Alice-Block_2048x.jpg?v=1517059999");
+ const sylvester = new Cat("sylvester", "Sylvester", "https://cdn.shopify.com/s/files/1/0673/5325/files/LadyDinahs_Cat_Cafe_Alice-Block_2048x.jpg?v=1517059999");
 
- const grumpyCat = new Cat("Grumpy Cat", "https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg");
+ const grumpy = new Cat("grumpy", "Grumpy", "https://pbs.twimg.com/profile_images/948294484596375552/RyGNqDEM_400x400.jpg");
 
 
  //Elements for each Cat
  sylvester.buildDOMCat();
- grumpyCat.buildDOMCat();
+ grumpy.buildDOMCat();
 
 
- //Click Function
- $(".cat-button-article").on("click", ".cat-image", (e) => {
-   sylvester.clicks += 1;
-   grumpyCat.clicks += 1;
-   $(".clicks").text(grumpyCat.clicks);
+ //Click Functions
+ //Sylvester
+ $(".cat-button-article").on("click", "#sylvester", (e) => {
+   sylvester.increaseClick();
  });
+
+ $(".cat-button-article").on("click", "#grumpy", (e) => {
+   grumpy.increaseClick();
+ });
+
+
 
 })
